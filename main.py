0d090529a86d126a475f17bcb58bc1bc33fbe33b
Matheus Pintor Fernandes Ferreira <mathfer1000@gmail.com>
2026-08-04 21:32:54 -0300

    chore: remover arquivo na linguagem errada da equipe

--- main.py
+++
@@ -1,7 +0,0 @@
-num = int(input("Informe um número: "))
-print(f"Opa! Vou mostrar a tabuada do {num} para você \n")
-
-print("==============")
-for i in range(1, 11):
-    print(f"{num} x {i} = {num * i}")
-print("==============")
